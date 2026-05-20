4469fe1ef56b7f285d9645c8260a064e00eb10ed
Notlvy <szekely.timea@gmail.com>
2026-05-20 10:21:58 +0200

    create superuser on deploy

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.filter(username='ivyadmins').exists() or User.objects.create_superuser('ivyadmin', 'szekely.timea@gmail.com', '3463')" | python manage.py shell
