8e58e59bd50da28c00f5f47660e859790c5d0e93
Notlvy <szekely.timea@gmail.com>
2026-05-20 10:13:05 +0200

    revert superuser management commands

--- build.sh
+++ build.sh
@@ -2,14 +2,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth.models import User
-import os
-username = os.environ.get('ADMIN_USER', 'ivyadmin')
-password = os.environ.get('ADMIN_PASSWORD', '')
-email = os.environ.get('ADMIN_EMAIL', '')
-if password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print('Superuser created!')
-else:
-    print('Superuser already exists or no password set.')
